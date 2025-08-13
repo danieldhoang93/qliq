7a7d4cc55a46ec72bb843d631d963bd45045f68f
Daniel Hoang <danieldhoang93@gmail.com>
2025-08-13 00:47:28 -0700

    wip: new ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
     browserName: 'chromium', // force Chromium
     headless: true, // always run headless
   },
+  webServer: {
+    // if Vite: add --host to avoid binding issues in CI
+    command: 'npm run dev -- --host',
+    url: 'http://localhost:5173',
+    timeout: 120_000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
