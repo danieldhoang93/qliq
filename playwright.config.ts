8308c57628fdaedf66ccec0414fc1759fd17fdd5
Daniel Hoang <42419362+danieldhoang93@users.noreply.github.com>
2025-08-13 01:05:19 -0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,18 +3,16 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: 'src/tests/e2e',
   testMatch: '**/*.spec.ts',
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'], channel: 'chromium' },
-    },
-  ],
+
   use: {
-    // browserName: 'chromium', // force Chromium
-    headless: true, // always run headless
+    ...devices['Desktop Chrome'],
+    channel: 'chromium', // new headless mode
+    headless: true,
+    baseURL: 'http://localhost:5173',
   },
+
   webServer: {
-    command: 'npm run dev -- --host', // Vite: ensure it binds properly
+    command: 'npm run dev -- --host',
     url: 'http://localhost:5173',
     timeout: 120_000,
     reuseExistingServer: false,
